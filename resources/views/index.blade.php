6955fbbf211c3113273b6b865d1df18333eb5c95
ArionoS <arionoseptian0802@gmail.com>
2021-03-05 15:29:06 +0700

    Arjen1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,22 +10,22 @@ License URI: http://www.themevault.net/license/-->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="icon" href="images/icons/favicon.png"/>
+        <link rel="icon" href="a/images/icons/favicon.png"/>
         <title>vacayhome</title>
 
         <!-- Bootstrap core CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="a/css/bootstrap.min.css" rel="stylesheet">
+        <link href="a/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <!-- Custom styles for this template -->
-        <link href="css/style.css" rel="stylesheet">
-        <link href="fonts/antonio-exotic/stylesheet.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/lightbox.min.css">
-        <link href="css/responsive.css" rel="stylesheet">
-        <script src="js/jquery.min.js" type="text/javascript"></script>
-        <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="js/lightbox-plus-jquery.min.js" type="text/javascript"></script>
-        <script src="js/instafeed.min.js" type="text/javascript"></script>
-        <script src="js/custom.js" type="text/javascript"></script>
+        <link href="a/css/style.css" rel="stylesheet">
+        <link href="a/fonts/antonio-exotic/stylesheet.css" rel="stylesheet">
+        <link rel="stylesheet" href="a/css/lightbox.min.css">
+        <link href="a/css/responsive.css" rel="stylesheet">
+        <script src="a/js/jquery.min.js" type="text/javascript"></script>
+        <script src="a/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="a/js/lightbox-plus-jquery.min.js" type="text/javascript"></script>
+        <script src="a/js/instafeed.min.js" type="text/javascript"></script>
+        <script src="a/js/custom.js" type="text/javascript"></script>
     </head>
     <body>
         <div id="page">
@@ -108,25 +108,25 @@ License URI: http://www.themevault.net/license/-->
                     <li data-target="#myCarousel1" data-slide-to="2"></li>
                 </ol>
                 <div class="carousel-inner">
-                    <div class="item active"> <img src="images/banner.png" style="width:100%; height: 500px" alt="First slide">
+                    <div class="item active"> <img src="a/images/banner.png" style="width:100%; height: 500px" alt="First slide">
                         <div class="carousel-caption">
                             <h1>vacayhome<br> spa & Resort</h1>
                         </div>
                     </div>
-                    <div class="item"> <img src="images/banner2.png" style="width:100%; height: 500px" alt="Second slide">
+                    <div class="item"> <img src="a/images/banner2.png" style="width:100%; height: 500px" alt="Second slide">
                         <div class="carousel-caption">
                             <h1>vacayhome<br> spa & Resort</h1>
                         </div>
                     </div>
-                    <div class="item"> <img src="images/banner3.png" style="width:100%; height: 500px" alt="Third slide">
+                    <div class="item"> <img src="a/images/banner3.png" style="width:100%; height: 500px" alt="Third slide">
                         <div class="carousel-caption">
                             <h1>vacayhome<br> spa & Resort</h1>
                         </div>
                     </div>
 
                 </div>
-                <a class="left carousel-control" href="#myCarousel1" data-slide="prev"> <img src="images/icons/left-arrow.png" onmouseover="this.src = 'images/icons/left-arrow-hover.png'" onmouseout="this.src = 'images/icons/left-arrow.png'" alt="left"></a>
-                <a class="right carousel-control" href="#myCarousel1" data-slide="next"><img src="images/icons/right-arrow.png" onmouseover="this.src = 'images/icons/right-arrow-hover.png'" onmouseout="this.src = 'images/icons/right-arrow.png'" alt="left"></a>
+                <a class="left carousel-control" href="#myCarousel1" data-slide="prev"> <img src="a/images/icons/left-arrow.png" onmouseover="this.src = 'images/icons/left-arrow-hover.png'" onmouseout="this.src = 'images/icons/left-arrow.png'" alt="left"></a>
+                <a class="right carousel-control" href="#myCarousel1" data-slide="next"><img src="a/images/icons/right-arrow.png" onmouseover="this.src = 'images/icons/right-arrow-hover.png'" onmouseout="this.src = 'images/icons/right-arrow.png'" alt="left"></a>
 
             </div>
             <div class="clearfix"></div>
@@ -138,7 +138,7 @@ License URI: http://www.themevault.net/license/-->
                         <div class="col-md-3 col-sm-3 col-xs-6 width-50">
                             <div class="service-details text-center">
                                 <div class="service-image">
-                                    <img alt="image" class="img-responsive" src="images/icons/wifi.png">
+                                    <img alt="image" class="a/img-responsive" src="a/images/icons/wifi.png">
                                 </div>
                                 <h4><a>free wifi</a></h4>
                             </div>
@@ -146,7 +146,7 @@ License URI: http://www.themevault.net/license/-->
                         <div class="col-md-3 col-sm-3 col-xs-6 width-50">
                             <div class="service-details text-center">
                                 <div class="service-image">
-                                    <img alt="image" class="img-responsive" src="images/icons/key.png">
+                                    <img alt="image" class="img-responsive" src="a/images/icons/key.png">
                                 </div>
                                 <h4><a>room service</a></h4>
                             </div>
@@ -154,7 +154,7 @@ License URI: http://www.themevault.net/license/-->
                         <div class="col-md-3 col-sm-3 col-xs-6 mt-25">
                             <div class="service-details text-center">
                                 <div class="service-image">
-                                    <img alt="image" class="img-responsive" src="images/icons/car.png">
+                                    <img alt="image" class="img-responsive" src="a/images/icons/car.png">
                                 </div>
                                 <h4><a>free parking</a></h4>
                             </div>
@@ -162,7 +162,7 @@ License URI: http://www.themevault.net/license/-->
                         <div class="col-md-3 col-sm-3 col-xs-6 mt-25">
                             <div class="service-details text-center">
                                 <div class="service-image">
-                                    <img alt="image" class="img-responsive" src="images/icons/user.png">
+                                    <img alt="image" class="img-responsive" src="a/images/icons/user.png">
                                 </div>
                                 <h4><a>customer support</a></h4>
                             </div>
@@ -177,36 +177,36 @@ License URI: http://www.themevault.net/license/-->
                     <div class="row">
                         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                             <div class="gallery-image">
-                                <img class="img-responsive" src="images/room1.png">
+                                <img class="img-responsive" src="a/images/room1.png">
                                 <div class="overlay">
-                                    <a class="info pop example-image-link img-responsive" href="images/room1.png" data-lightbox="example-1"><i class="fa fa-search" aria-hidden="true"></i></a>
+                                    <a class="info pop example-image-link img-responsive" href="a/images/room1.png" data-lightbox="example-1"><i class="fa fa-search" aria-hidden="true"></i></a>
                                     <p><a>delux room</a></p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                             <div class="gallery-image">
-                                <img class="img-responsive" src="images/room2.png">
+                                <img class="img-responsive" src="a/images/room2.png">
                                 <div class="overlay">
-                                    <a class="info pop example-image-link img-responsive" href="images/room2.png" data-lightbox="example-1"><i class="fa fa-search" aria-hidden="true"></i></a>
+                                    <a class="info pop example-image-link img-responsive" href="a/images/room2.png" data-lightbox="example-1"><i class="fa fa-search" aria-hidden="true"></i></a>
                                     <p><a>super room</a></p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                             <div class="gallery-image">
-                                <img class="img-responsive" src="images/room3.png">
+                                <img class="img-responsive" src="a/images/room3.png">
                                 <div class="overlay">
-                                    <a class="info pop example-image-link img-responsive" href="images/room3.png" data-lightbox="example-1"><i class="fa fa-search" aria-hidden="true"></i></a>
+                                    <a class="info pop example-image-link img-responsive" href="a/images/room3.png" data-lightbox="example-1"><i class="fa fa-search" aria-hidden="true"></i></a>
                                     <p><a>single room</a></p>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
                             <div class="gallery-image">
-                                <img class="img-responsive" src="images/room4.png">
+                                <img class="img-responsive" src="a/images/room4.png">
                                 <div class="overlay">
-                                    <a class="info pop example-image-link img-responsive" href="images/room4.png" data-lightbox="example-1"><i class="fa fa-search" aria-hidden="true"></i></a>
+                                    <a class="info pop example-image-link img-responsive" href="a/images/room4.png" data-lightbox="example-1"><i class="fa fa-search" aria-hidden="true"></i></a>
                                     <p><a>double room</a></p>
                                 </div>
                             </div>
@@ -241,7 +241,7 @@ License URI: http://www.themevault.net/license/-->
                             <div class="side-A">
                                 <div class="product-thumb">
                                     <div class="image">
-                                        <a><img src="images/category1.png" class="img-responsive" alt="image"></a>
+                                        <a><img src="a/images/category1.png" class="img-responsive" alt="image"></a>
                                     </div>
                                 </div>
                             </div>
@@ -258,7 +258,7 @@ License URI: http://www.themevault.net/license/-->
                             <div class="side-A">
                                 <div class="product-thumb">
                                     <div class="image">
-                                        <a><img alt="image" class="img-responsive" src="images/category2.png"></a>
+                                        <a><img alt="image" class="img-responsive" src="a/images/category2.png"></a>
                                     </div>
                                 </div>
                             </div>
@@ -283,7 +283,7 @@ License URI: http://www.themevault.net/license/-->
                             <div class="side-B">
                                 <div class="product-thumb">
                                     <div class="image txt-rgt">
-                                        <a class="arrow-left"><img src="images/category3.png" class="img-responsive" alt="imaga"></a>
+                                        <a class="arrow-left"><img src="a/images/category3.png" class="img-responsive" alt="imaga"></a>
                                     </div>
                                 </div>
                             </div>
@@ -301,7 +301,7 @@ License URI: http://www.themevault.net/license/-->
                             <div class="side-B">
                                 <div class="product-thumb txt-rgt">
                                     <div class="image">
-                                        <a class="arrow-left"><img src="images/category4.png" class="img-responsive" alt="imaga"></a>
+                                        <a class="arrow-left"><img src="a/images/category4.png" class="img-responsive" alt="imaga"></a>
                                     </div>
                                 </div>
                             </div>
@@ -332,7 +332,7 @@ License URI: http://www.themevault.net/license/-->
                                     <div class="blog-view-box">
                                         <div class="media">
                                             <div class="media-left">
-                                                <img src="images/client1.png" class="media-object">
+                                                <img src="a/images/client1.png" class="media-object">
                                             </div>
                                             <div class="media-body">
                                                 <h3 class="media-heading blog-title">Walter Hucko</h3>
@@ -348,7 +348,7 @@ License URI: http://www.themevault.net/license/-->
                                     <div class="blog-view-box">
                                         <div class="media">
                                             <div class="media-left">
-                                                <img src="images/client2.png" class="media-object">
+                                                <img src="a/images/client2.png" class="media-object">
                                             </div>
                                             <div class="media-body">
                                                 <h3 class="media-heading blog-title">Jules Boutin</h3>
@@ -364,7 +364,7 @@ License URI: http://www.themevault.net/license/-->
                                     <div class="blog-view-box">
                                         <div class="media">
                                             <div class="media-left">
-                                                <img src="images/client3.png" class="media-object">
+                                                <img src="a/images/client3.png" class="media-object">
                                             </div>
                                             <div class="media-body">
                                                 <h3 class="media-heading blog-title">Attilio Marzi</h3>
@@ -387,7 +387,7 @@ License URI: http://www.themevault.net/license/-->
                         <div class="view-set-block">
                             <div class="col-md-6 col-sm-6 col-xs-12">
                                 <div class="event-blog-image">
-                                    <img alt="image" class="img-responsive" src="images/blog1.png">
+                                    <img alt="image" class="img-responsive" src="a/images/blog1.png">
                                 </div>
                             </div>
                             <div class="col-md-6 col-sm-6 col-xs-12 side-in-image">
@@ -405,7 +405,7 @@ License URI: http://www.themevault.net/license/-->
                         <div class="view-set-block">
                             <div class="col-md-6 col-sm-6 col-xs-12">
                                 <div class="event-blog-image">
-                                    <img alt="image" class="img-responsive" src="images/blog2.png">
+                                    <img alt="image" class="img-responsive" src="a/images/blog2.png">
                                 </div>
                             </div>
                             <div class="col-md-6 col-sm-6 col-xs-12 side-in-image">
